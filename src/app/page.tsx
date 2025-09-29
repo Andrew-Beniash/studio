6b0f3aab9fd07f3b0ba3972fb887a41c1dc1f762
Andrew Beniash <AndreyBenyash@gmail.com>
2025-09-29 20:16:15 +0000

    Change background color to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-background">
+    <div className="flex flex-col min-h-screen bg-white">
       <Header avatarUrl={avatarUrl} />
       <main className="flex-grow pt-28 pb-12 container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-lg mx-auto">
