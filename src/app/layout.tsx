202203ebb54c710e9decbf3789999ab95ee473c7
Andrew Beniash <AndreyBenyash@gmail.com>
2025-09-30 01:29:04 +0000

    the panel shall be close by default (_for element <SidebarContent>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">
-        <SidebarProvider>
+        <SidebarProvider defaultOpen={false}>
           <Sidebar>
             <SidebarHeader>
               {/* Sidebar Header Content */}
