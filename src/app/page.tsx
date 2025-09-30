dc4df361c190118dfa3164fcea82f059ce732f3a
Andrew Beniash <AndreyBenyash@gmail.com>
2025-09-30 00:21:28 +0000

    use white backgorund color  (_for element <Home>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ const getPriorityBadgeVariant = (priority: string) => {
 export default function Home() {
 
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow pt-16 pb-12 container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
