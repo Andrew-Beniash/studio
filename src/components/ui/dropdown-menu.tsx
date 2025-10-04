c307b89c8c27b92c4d18eca4054a44c5d776173a
Andrew Beniash <AndreyBenyash@gmail.com>
2025-10-04 14:38:55 +0000

    use the following style for this element .menu-heading {   font-family:

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -148,7 +148,7 @@ const DropdownMenuLabel = React.forwardRef<
   <DropdownMenuPrimitive.Label
     ref={ref}
     className={cn(
-      "px-2 py-1.5 text-sm font-semibold",
+      "py-2.5 px-4 text-[11px] font-semibold tracking-wider uppercase text-black/60 border-b border-black/5",
       inset && "pl-8",
       className
     )}
