3fe1d3f9fcd188abcfde723a495f8751003f09bb
Your Name <AndreyBenyash@gmail.com>
2025-10-04 14:15:28 +0000

    make it 60px wider (_for element <Primitive.div>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -45,7 +45,7 @@ export function Header({ onTitleClick, selectedTask }: HeaderProps) {
                   <ChevronDown className="h-4 w-4 text-white" />
                 </div>
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-64">
+              <DropdownMenuContent className="w-80">
                 <div className="p-2">
                     <Input placeholder="Search client data" />
                 </div>
