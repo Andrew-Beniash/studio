09f3e549079cabf6d3e2be8107249b764b39edd4
Andrew Beniash <AndreyBenyash@gmail.com>
2025-10-04 16:53:16 +0000

    update opacity 1.0 (_for element <Primitive.div>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -608,7 +608,7 @@ export default function Home() {
         </button>
       )}
        <AlertDialog open={isOpportunityAnalysisOpen} onOpenChange={setIsOpportunityAnalysisOpen}>
-        <AlertDialogContent className="max-w-3xl pb-1 opacity-90">
+        <AlertDialogContent className="max-w-3xl pb-1">
           <AlertDialogHeader>
             <div className="flex justify-between items-center">
               <div>
@@ -632,6 +632,8 @@ export default function Home() {
     </div>
   );
 }
+
+    
 
     
 
